66b1f975df7d88165708bc60e908f7ee390c3aee
Srishti Sharma <srishti.sharma@cybergindia.local>
2021-04-15 16:39:29 +0530

    changes

---
+++ script.js
@@ -0,0 +1,12 @@
+function gmail(){
+    window.open("mailto: sharmasrishti121098@gmail.com");
+}
+
+function linkedin(){
+    window.open("https://www.linkedin.com/in/srishti-sharma-a57bb9147");
+
+}
+
+function github(){
+    window.open("https://github.com/srishtisharma12");
+}
